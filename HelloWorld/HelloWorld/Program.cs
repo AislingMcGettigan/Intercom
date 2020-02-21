27af91e270260c829266ef2885808594d84498df
aisling mcgettigan <aislingmcgettigan93@hotmail.com>
2020-02-20 20:14:52 -0800

    Add project files.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrize = 29.95f;
+            char character = 'a';
+            string firstName = "Mosh";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrize);
+        }
+    }
+}
---
+++ StopWatch1/StopWatch1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Thread.Sleep(3000);
+
+            stopWatch.Stop();
+
+            Console.WriteLine("Duration =" + stopWatch.GetInterval());
+            Console.ReadLine();
+        }
+    }
+}
